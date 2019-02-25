5a6f5dd32d198e5035a677fae8002daf2b5711cf
MotohashiTomoya <MotohashiTomoya@users.noreply.github.com>
2019-02-25 18:27:00 +0900

    Q1

--- Q1.sql
+++ Q1.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE one;
-USE one;
+CREATE DATABASE only DEFAULT CHARACTER SET utf8;
+USE only;
 CREATE TABLE item_category(category_id int PRIMARY KEY AUTO_INCREMENT,category_name varchar(256) NOT NULL);
